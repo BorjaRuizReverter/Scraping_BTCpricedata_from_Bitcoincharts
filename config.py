2c4ab32cef982c4ae12364264022e5e2ff9fbfd5
BorjaRuizReverter <borjaruizreverter@gmail.com>
2023-08-02 22:53:55 -0300

    Uploading config file

---
+++ config.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+#Data scraped is from one day before start_date
+start_date = date(2017, 7, 2)
+end_date = date(2017, 7, 4)
+
+#Some of main exchanges are: bitfinexUSD, bitstampUSD, krakenUSD or mtgoxUSD
+exchange = 'krakenUSD'
